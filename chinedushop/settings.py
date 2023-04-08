a48568b88396a25ce03c1b9f8a22a57f5c3c019a
sbendavid <samuelbendavid01@gmail.com>
2023-04-08 02:37:53 +0100

    a little bit of styling

--- chinedushop/settings.py
+++ chinedushop/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(r#(t+uhbpi%()p_b+6%6(%+623x&w+ufz*7ni^n7u@v*ru+r#'
+SECRET_KEY = os.getenv("DjANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
