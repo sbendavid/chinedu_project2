db4ead688a9875ae44ac24528f6a0b60059b563f
sbendavid <samuelbendavid01@gmail.com>
2023-04-20 02:29:05 +0100

    it is working now

--- shop/views.py
+++ shop/views.py
@@ -12,104 +12,135 @@ from .cart import Cart
 # Create your views here.
 
 def product_list(request, category_slug=None):
-    category = None
-    categories = Category.objects.all()
-    products = Product.objects.filter(available=True)
+    try:
+        category = None
+        categories = Category.objects.all()
+        products = Product.objects.filter(available=True)
 
-    if category_slug:
-        category = get_object_or_404(Category, slug=category_slug)
-        products = products.filter(category=category)
+        if category_slug:
+            category = get_object_or_404(Category, slug=category_slug)
+            products = products.filter(category=category)
 
-    return render(
-        request, 'shop/product/list.html',
-        {
-        'category': category,
-        'categories': categories,
-        'products': products
-        })
+        return render(
+            request, 'shop/product/list.html',
+            {
+            'category': category,
+            'categories': categories,
+            'products': products
+            })
+    except Exception as e:
+        return HttpResponse("Error: {}".format(str(e)), status=500)
+
 
 def product_detail(request, id, slug):
-    product = get_object_or_404(Product,
-    id=id,
-    slug=slug,
-    available=True
-    )
-    cart_product_form = CartAddProductForm()
-    return render(
-        request, 'shop/product/detail.html',
-        {'product': product,
-         'cart_product_form': cart_product_form
-         })
+    try:
+        product = get_object_or_404(Product,
+        id=id,
+        slug=slug,
+        available=True
+        )
+        cart_product_form = CartAddProductForm()
+        return render(
+            request, 'shop/product/detail.html',
+            {'product': product,
+            'cart_product_form': cart_product_form
+            })
+    except Exception as e:
+        return HttpResponse("Error: {}".format(str(e)), status=500)
+    
 
 def product_search(request):
-    form = SearchForm()
-    query = None
-    results = []
-    if 'query' in request.GET:
-        form = SearchForm(request.GET)
-        if form.is_valid():
-            query = form.cleaned_data['query']
-            results = Product.objects.annotate(
-                search=SearchVector('name'),
-                ).filter(search=query)
-    return render(request,
-                    'shop/product/search.html',
-                    {'form': form,
-                    'query': query,
-                    'results': results})
+    try:
+        form = SearchForm()
+        query = None
+        results = []
+        if 'query' in request.GET:
+            form = SearchForm(request.GET)
+            if form.is_valid():
+                query = form.cleaned_data['query']
+                results = Product.objects.annotate(
+                    search=SearchVector('name'),
+                    ).filter(search=query)
+        return render(request,
+                        'shop/product/search.html',
+                        {'form': form,
+                        'query': query,
+                        'results': results})
+    except Exception as e:
+        return HttpResponse("Error: {}".format(str(e)), status=500)
+    
 
 @login_required
 def dashboard(request):
-    return render(
-        request, 'shop/product/dashboard.html',
-        {'section': 'dashboard'}
-        )
+    try:
+        return render(
+            request, 'shop/product/dashboard.html',
+            {'section': 'dashboard'}
+            )
+    except Exception as e:
+        return HttpResponse("Error: {}".format(str(e)), status=500)
+    
 
 @require_POST
 def cart_add(request, product_id):
-    cart = Cart(request)
-    product = get_object_or_404(Product, id=product_id)
-    form = CartAddProductForm(request.POST)
-    if form.is_valid():
-        cd = form.cleaned_data
-        cart.add(
-            product=product, 
-            quantity=cd['quantity'], 
-            update_quantity=cd['update'])
-    return redirect('cart_detail')
+    try:
+        cart = Cart(request)
+        product = get_object_or_404(Product, id=product_id)
+        form = CartAddProductForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            cart.add(
+                product=product, 
+                quantity=cd['quantity'], 
+                update_quantity=cd['update'])
+        return redirect('cart_detail')
+    except Exception as e:
+        return HttpResponse("Error: {}".format(str(e)), status=500)
+    
 
 def cart_remove(request, product_id):
-    cart = Cart(request)
-    product = get_object_or_404(Product, id=product_id)
-    cart.remove(product)
-    return redirect('cart_detail')
+    try:
+        cart = Cart(request)
+        product = get_object_or_404(Product, id=product_id)
+        cart.remove(product)
+        return redirect('cart_detail')
+    except Exception as e:
+        return HttpResponse("Error: {}".format(str(e)), status=500)
+    
 
 def cart_detail(request):
-    cart = Cart(request)
-    for item in cart:
-        item['update_quantity_form'] = CartAddProductForm(
-            initial={'quantity': item['quantity'], 
-                     'update': True})
-    return render(request, 'cart/detail.html', {'cart': cart})
+    try:
+        cart = Cart(request)
+        for item in cart:
+            item['update_quantity_form'] = CartAddProductForm(
+                initial={'quantity': item['quantity'], 
+                        'update': True})
+        return render(request, 'cart/detail.html', {'cart': cart})
+    except Exception as e:
+        return HttpResponse("Error: {}".format(str(e)), status=500)
+    
 
 def order_create(request):
-    cart = Cart(request)
-    if request.method == 'POST':
-        form = OrderCreateForm(request.POST)
-        if form.is_valid():
-            order = form.save()
-            for item in cart:
-                OrderItem.objects.create(order=order,
-                product=item['product'],
-                price=item['price'],
-                quantity=item['quantity'])
-            # clear the cart
-            cart.clear()
-        return render(request,
-                    'orders/order/created.html',
-                    {'order': order})
-    else:
-        form = OrderCreateForm()
-    return render(request, 
-                  'orders/order/create.html', 
-                  {'cart': cart, 'form': form})
+    try:
+        cart = Cart(request)
+        if request.method == 'POST':
+            form = OrderCreateForm(request.POST)
+            if form.is_valid():
+                order = form.save()
+                for item in cart:
+                    OrderItem.objects.create(order=order,
+                    product=item['product'],
+                    price=item['price'],
+                    quantity=item['quantity'])
+                # clear the cart
+                cart.clear()
+            return render(request,
+                        'orders/order/created.html',
+                        {'order': order})
+        else:
+            form = OrderCreateForm()
+        return render(request, 
+                    'orders/order/create.html', 
+                    {'cart': cart, 'form': form})
+    except Exception as e:
+        return HttpResponse("Error: {}".format(str(e)), status=500)
